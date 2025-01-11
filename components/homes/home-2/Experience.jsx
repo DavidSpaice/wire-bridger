be164e1b5ce4572b57755274308d04444c57a41a
DavidSpaice <support@airtekontario.com>
2025-01-11 11:43:37 -0500

    feat: modify experience component content

--- components/homes/home-2/Experience.jsx
+++ components/homes/home-2/Experience.jsx
@@ -6,24 +6,24 @@ export default function Experience() {
       {/* Marquee Text Line */}
       <div className="marquee marquee-style-2">
         <div className="marquee-track marquee-animation-1a">
-          <div>LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
-          <div aria-hidden="true">LIFETIME WARRANTY</div>
+          <div>LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
+          <div aria-hidden="true">LIFETIME LIMITED WARRANTY</div>
         </div>
       </div>
       {/* End Marquee Text Line */}
