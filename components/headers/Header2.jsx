111e9a1716b0a817e21b6d03b51ea02afb4c2e23
DavidSpaice <support@airtekontario.com>
2025-02-18 13:58:26 -0500

    feat: add space on logo

--- components/headers/Header2.jsx
+++ components/headers/Header2.jsx
@@ -80,7 +80,7 @@ export default function Header2({ links }) {
                       margin: 0,
                     }}
                   >
-                    WIRE BRIDGER
+                    WIRE &nbsp;BRIDGER
                   </h1>{" "}
                 </div>
                 <div className="light-mode-logo" style={{ width: "100%" }}>
@@ -153,7 +153,7 @@ export default function Header2({ links }) {
                       margin: 0,
                     }}
                   >
-                    WIRE BRIDGER
+                    WIRE &nbsp;BRIDGER
                   </h1>{" "}
                 </div>
                 <div className="dark-mode-logo" style={{ width: "100%" }}>
